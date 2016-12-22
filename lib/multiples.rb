89d97d4fcd3c249ba4b53894b97a6c5ad6fb6b6d
lshapz <laurashapiro01@gmail.com>
2016-12-22 16:50:35 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+
+  (1..limit-1).each_with_object(Array.new(0)) do |x, arr|
+    if x % 3 == 0
+      arr.push(x)
+    elsif x % 5 == 0
+      arr.push(x)
+    end
+  end
+end
+
+def sum_multiples(limit)
+  answer = collect_multiples(limit)
+
+  answer.reduce(&:+)
+end 
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+attr_accessor :limit 
+
+def initialize(limit)
+  self.limit = limit
+end
+
+def collect_multiples
+   (1..self.limit-1).each_with_object(Array.new(0)) do |x, arr|
+    if x % 3 == 0
+      arr.push(x)
+    elsif x % 5 == 0
+      arr.push(x)
+    end
+  end
+end
+
+def sum_multiples
+  answer = self.collect_multiples
+
+  answer.reduce(&:+)
+end 
+
+end
